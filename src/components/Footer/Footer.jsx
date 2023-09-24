37fd39738801742c93b321a6e271e8d8e89cbc1f
Thế Nghĩa <nghiask2018@gmail.com>
2023-09-24 12:19:46 +0700

    Update Footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,8 +1,21 @@
 
+import Link from "next/link";
+
 const Footer = () => {
     return (
-        <div id="footer">
-            <h1 className="py-10 bg-stone-950 text-center text-white">This is footer</h1>
+        <div className='bg-stone-950 pt-6 pb-10 sm:py-8'>
+            <div className='section-container-no-py flex flex-col-reverse sm:flex-row items-center justify-between'>
+                <Link href="/" className="flex items-center mt-8 sm:mt-0">
+                    <img src='/logo.png' className="h-11 mr-2" alt="Logo" />
+                    <img src='/joyfilm.png' className="h-10" alt="JoyFilm" />
+                </Link>
+                <div className='flex w-full justify-evenly sm:w-auto items-center'>
+                    <Link href='/gioi-thieu' className='footer-link'>Giới thiệu</Link>
+                    <Link href='https://github.com/dothenghia/JoyFilm-Web' target='_blank' className='footer-link'>Github</Link>
+                    <Link href='https://github.com/dothenghia/JoyFilm-Server' target='_blank' className='footer-link'>API</Link>
+                    <Link href='/gioi-thieu' className='footer-link'>Liên hệ</Link>
+                </div>
+            </div>
         </div>
     );
 };
